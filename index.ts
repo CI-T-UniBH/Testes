e392e4d27222dd6eb6d8070f023dfa144599cece
Edu Caribé <edu.caribe@hotmail.com>
2021-12-02 10:25:38 -0300

    refactor: agrupar logs

--- index.ts
+++ index.ts
@@ -79,28 +79,30 @@ class DesafioQACIT {
      * Inicia o primeiro caso de testes
      */
     private async startFirstCase(): Promise<void> {
-        console.log('\n[1º caso] Busca no Banco de Questões');
+        console.group('\n[1º caso] Busca no Banco de Questões');
 
-        console.log('- Abrindo página inicial');
+        console.log('Abrindo página inicial');
         await this.browseBaseURL();
 
-        console.log('- Pressionando botão de busca');
+        console.log('Pressionando botão de busca');
         await (await this.driver!.findElement(By.partialLinkText('BROWSE'))).click();
         
-        console.log('- Aguardando redirecionamento');
+        console.log('Aguardando redirecionamento');
         await this.awaitPageRedirects('https://opentdb.com/browse.php', 10000);
         
-        console.log('- Inserindo o termo desejado "Science: Computers" no campo de busca');
+        console.log('Inserindo o termo desejado "Science: Computers" no campo de busca');
         await (await this.driver!.findElement(By.id('query'))).sendKeys('Science: Computers');
 
-        console.log('- Pressionando botão de filtro');
+        console.log('Pressionando botão de filtro');
         await (await this.driver!.findElement(By.css('#query ~ button'))).click();
 
-        console.log('- Aguardando resultados');
+        console.log('Aguardando resultados');
         await this.awaitPageRedirects('https://opentdb.com/browse.php?query=Science%3A+Computers&type=Question#');
 
-        console.log('- Buscando mensagem de erro "No questions found."');
+        console.log('Buscando mensagem de erro "No questions found."');
         const alertMessage: string = await (await this.driver!.findElement(By.css('.container > .alert.alert-danger'))).getText();
+        
+        console.groupEnd();
 
         if (alertMessage === 'No questions found.') {
             console.log('[SUCESSO] Teste validado com êxito.');
@@ -116,35 +118,37 @@ class DesafioQACIT {
      * Inicia o segundo caso de testes
      */
      private async startSecondCase(): Promise<void> {
-        console.log('\n[2º caso] Busca no Banco de Categorias');
+        console.group('\n[2º caso] Busca no Banco de Categorias');
 
-        console.log('- Abrindo página inicial');
+        console.log('Abrindo página inicial');
         await this.browseBaseURL();
 
-        console.log('- Pressionando botão de busca');
+        console.log('Pressionando botão de busca');
         await (await this.driver!.findElement(By.partialLinkText('BROWSE'))).click();
         
-        console.log('- Aguardando redirecionamento');
+        console.log('Aguardando redirecionamento');
         await this.awaitPageRedirects('https://opentdb.com/browse.php', 10000);
         
-        console.log('- Inserindo o termo desejado "Science: Computers" no campo de busca');
+        console.log('Inserindo o termo desejado "Science: Computers" no campo de busca');
         await (await this.driver!.findElement(By.id('query'))).sendKeys('Science: Computers');
 
-        console.log('- Alterando tipo de filtragem de "Question" para "Category"');
+        console.log('Alterando tipo de filtragem de "Question" para "Category"');
         await (await this.driver!.findElement(By.id('type'))).sendKeys(Key.ENTER, Key.ARROW_DOWN, Key.ARROW_DOWN, Key.ENTER);
 
-        console.log('- Pressionando botão de filtro');
+        console.log('Pressionando botão de filtro');
         await (await this.driver!.findElement(By.css('#query ~ button'))).click();
 
-        console.log('- Aguardando resultados');
+        console.log('Aguardando resultados');
         await this.awaitPageRedirects('https://opentdb.com/browse.php?query=Science%3A+Computers&type=Category#');
 
-        console.log('- Verificando quantidade (esperado 25 resultados)');
+        console.log('Verificando quantidade (esperado 25 resultados)');
         const table: WebElement = await this.driver!.findElement(By.css('.container > h2 ~ .table'));
         const rowsCount: number = await (await table.findElements(By.css('tbody > tr'))).length ;
         
-        console.log('- Verificando se o controle de paginação está visível');
+        console.log('Verificando se o controle de paginação está visível');
         const paginationVisible: boolean = await (await this.driver!.findElement(By.css('.pagination'))).isDisplayed();
+        
+        console.groupEnd();
 
         if (rowsCount === 25 && paginationVisible) {
             console.log('[SUCESSO] Teste validado com êxito.');
@@ -160,28 +164,30 @@ class DesafioQACIT {
      * Inicia o terceiro caso de testes
      */
      private async startThirdCase(): Promise<void> {
-        console.log('\n[3º caso] Login com Credenciais Inválidas');
+        console.group('\n[3º caso] Login com Credenciais Inválidas');
 
-        console.log('- Abrindo página inicial');
+        console.log('Abrindo página inicial');
         await this.browseBaseURL();
 
-        console.log('- Pressionando botão de login');
+        console.log('Pressionando botão de login');
         await (await this.driver!.findElement(By.partialLinkText('LOGIN'))).click();
         
-        console.log('- Aguardando redirecionamento');
+        console.log('Aguardando redirecionamento');
         await this.awaitPageRedirects('https://opentdb.com/login.php', 10000);
 
-        console.log('- Inserindo o nome de usuário inexistente "UsuarioTeste" no campo de nome');
+        console.log('Inserindo o nome de usuário inexistente "UsuarioTeste" no campo de nome');
         await (await this.driver!.findElement(By.id('username'))).sendKeys('UsuarioTeste');
         
-        console.log('- Inserindo senha inexistente "SenhaTeste" no campo de senha');
+        console.log('Inserindo senha inexistente "SenhaTeste" no campo de senha');
         await (await this.driver!.findElement(By.id('password'))).sendKeys('SenhaTeste');
         
-        console.log('- Enviando o formulário de login');
+        console.log('Enviando o formulário de login');
         await (await this.driver!.findElement(By.className('form-signin'))).submit();
 
-        console.log('- Buscando mensagem de erro "ERROR! Logging In Failed."');
+        console.log('Buscando mensagem de erro "ERROR! Logging In Failed."');
         const alertElement: WebElement = await (await this.driver!.wait(until.elementLocated(By.css('.container > .alert.alert-danger')), 5000));
+        
+        console.groupEnd();
 
         if (await alertElement.getText() === 'ERROR! Logging In Failed.') {
             console.log('[SUCESSO] Teste validado com êxito.');
